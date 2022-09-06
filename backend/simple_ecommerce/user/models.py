313c1464360f867a4c00cb09ba79619883de9de6
jkain88 <jkain88@yahoo.com>
2022-09-06 23:06:16 +0800

    Update user email field to unique

--- backend/simple_ecommerce/user/models.py
+++ backend/simple_ecommerce/user/models.py
@@ -29,7 +29,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser):
     age = models.PositiveIntegerField()
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     first_name = models.CharField(max_length=100)
