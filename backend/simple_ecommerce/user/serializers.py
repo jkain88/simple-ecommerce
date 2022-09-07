427d269312ce1c7c8e2e007eda90135a4b7ab8ff
jkain88 <jkain88@yahoo.com>
2022-09-07 22:53:41 +0800

    Update user serializer

--- backend/simple_ecommerce/user/serializers.py
+++ backend/simple_ecommerce/user/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import User
 
-class UserRegisterSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
@@ -11,7 +11,15 @@ class UserRegisterSerializer(serializers.ModelSerializer):
             'first_name',
             'middle_name',
             'last_name',
-            'password',
             'sex'
         ]
-        extra_kwargs = {'password': {'write_only': True}}
+
+
+class UserRegisterSerializer(UserSerializer):
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + ['password']
+        extra_kwargs = {
+            'password': {
+                'write_only': True, 
+            }
+        }
