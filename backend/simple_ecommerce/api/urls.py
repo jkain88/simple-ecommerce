781301cebe5cccfa21495c8f6963cc14cdee1de8
Martin <martinjosh@vashcorp.com>
2022-09-08 18:43:58 +0800

    Add jwt authentication

--- backend/simple_ecommerce/api/urls.py
+++ backend/simple_ecommerce/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
-    path('user/', include('simple_ecommerce.user.urls'))
+    path('user/', include('simple_ecommerce.user.urls')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
