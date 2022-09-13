11ecaba51f2a21d8162e50e08d8092658e7324ae
jkain88 <jkain88@yahoo.com>
2022-09-13 22:40:50 +0800

    Update existing endpoint naming convention

--- backend/simple_ecommerce/api/urls.py
+++ backend/simple_ecommerce/api/urls.py
@@ -6,7 +6,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
-    path('user/', include('simple_ecommerce.user.urls')),
+    path('products/', include('simple_ecommerce.product.urls')),
+    path('users/', include('simple_ecommerce.user.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/simple_ecommerce/user/urls.py
+++ backend/simple_ecommerce/user/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('address/', views.AddressView.as_view(), name='address'),
+    path('address/create/', views.AddressView.as_view(), name='address'),
     path('profile', views.UserProfileView.as_view(), name='profile'),
     path('register/', views.UserRegisterView.as_view(), name='register')
 ]
