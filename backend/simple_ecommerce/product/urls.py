4f5d617761decb1769e7d7cb72479cda57078617
jkain88 <jkain88@yahoo.com>
2022-09-16 22:27:49 +0800

    Fix details views endpoint

--- backend/simple_ecommerce/product/urls.py
+++ backend/simple_ecommerce/product/urls.py
@@ -6,9 +6,10 @@ from . import views
 urlpatterns = [
     path('', views.ProductList.as_view(), name='product_list'),
     path('categories', views.CategoryList.as_view(), name='category_list'),
-    path('category/<int:pk>', views.CategoryRetrieveUpdateDestroy.as_view(), name='category_retrieve_update_destory'),
+    path('category/<int:pk>', views.CategoryDetail.as_view(), name='category_detail'),
     path('category/create/', views.CategoryCreate.as_view(), name='category_create'),
     path('create/', views.ProductCreate.as_view(), name='product_create'),
-    path('<int:pk>', views.ProductRetrieveUpdateDestroy.as_view(), name='product_retrieve_update_destroy'),
-    path('variant/create/', views.ProductVariantCreate.as_view(), name='product_variant_create')
+    path('<int:pk>', views.ProductDetail.as_view(), name='product_detail'),
+    path('variant/create/', views.ProductVariantCreate.as_view(), name='product_variant_create'),
+    path('variant/<int:pk>', views.ProductVariantDetail.as_view(), name='product_variant_detail')
 ]
