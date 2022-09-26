352be2c8c37b91ed19b12d5662e991c9fc5867bd
jkain88 <jkain88@yahoo.com>
2022-09-26 21:23:08 +0800

    Add checkout detail view and endpoint

--- backend/simple_ecommerce/checkout/urls.py
+++ backend/simple_ecommerce/checkout/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.CheckoutDetail.as_view(), name='checkout_detail'),
     path('create/', views.CheckoutCreate.as_view(), name='checkout_create'),
     path('<int:pk>', views.CheckoutUpdate.as_view(), name='checkout_detail')
 ]
--- backend/simple_ecommerce/checkout/views.py
+++ backend/simple_ecommerce/checkout/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
@@ -15,3 +16,12 @@ class CheckoutUpdate(generics.UpdateAPIView):
     queryset = Checkout.objects.all()
     serializer_class = CheckoutSerializer
     permission_classes = [IsAuthenticated]
+
+
+class CheckoutDetail(generics.RetrieveAPIView):
+    serializer_class = CheckoutSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        obj = get_object_or_404(Checkout, user=self.request.user)
+        return obj
