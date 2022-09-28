1dcc3249b4ee09e69d1c8dee6aaff1bb2ce9c7a7
jkain88 <jkain88@yahoo.com>
2022-09-28 21:18:35 +0800

    Update checkout model remove status field

--- backend/simple_ecommerce/checkout/models.py
+++ backend/simple_ecommerce/checkout/models.py
@@ -5,7 +5,6 @@ from djmoney.models.fields import MoneyField
 from djmoney.money import Money
 from model_utils.models import TimeStampedModel
 
-from .choices import OrderStatus
 from .utils import get_line_amount
 from simple_ecommerce.product.models import (
     Product,
@@ -21,7 +20,6 @@ from simple_ecommerce.user.models import (
 class Checkout(TimeStampedModel):
     billing_address = models.ForeignKey(Address, null=True, on_delete=models.SET_NULL, related_name='+')
     shipping_address = models.ForeignKey(Address, null=True, on_delete=models.SET_NULL, related_name='+')
-    status = models.CharField(max_length=20, choices=OrderStatus.CHOICES, default=OrderStatus.PENDING)
     total_amount = MoneyField(max_digits=9, decimal_places=2, null=True, default_currency=settings.DEFAULT_CURRENCY)
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     
