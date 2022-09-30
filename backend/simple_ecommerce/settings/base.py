70357ea4a8006267be474eb3209f65c40b4f01b2
jkain88 <jkain88@yahoo.com>
2022-09-30 23:17:39 +0800

    Add pagination config

--- backend/simple_ecommerce/settings/base.py
+++ backend/simple_ecommerce/settings/base.py
@@ -124,12 +124,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# Rest framework config
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework_simplejwt.authentication.JWTAuthentication',
-#     )
-#}
+#Rest framework config
+REST_FRAMEWORK = {
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
 
 # Prices currency
 DEFAULT_CURRENCY = 'PHP'
