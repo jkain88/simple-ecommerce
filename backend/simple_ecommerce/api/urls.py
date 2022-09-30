1a99069f6e5765a10776bbb23b95f1f872ab7b34
jkain88 <jkain88@yahoo.com>
2022-09-30 23:18:04 +0800

    Add orders endpoint

--- backend/simple_ecommerce/api/urls.py
+++ backend/simple_ecommerce/api/urls.py
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('checkout/', include('simple_ecommerce.checkout.urls')),
+    path('orders/', include('simple_ecommerce.order.urls')),
     path('products/', include('simple_ecommerce.product.urls')),
     path('users/', include('simple_ecommerce.user.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
