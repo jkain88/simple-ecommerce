3ed95db98c04e45855e6b5c2da41119791abd1cd
Martin <martinjosh@vashcorp.com>
2022-10-10 18:12:47 +0800

    Add nextAuth session provider

--- storefront/pages/_app.tsx
+++ storefront/pages/_app.tsx
@@ -1,15 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Session } from 'next-auth';
 
 const queryClient = new QueryClient();
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
-  );
+function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
+    return (
+        <SessionProvider session={pageProps.session}>
+            <QueryClientProvider client={queryClient}>
+                <Component {...pageProps} />
+            </QueryClientProvider>
+        </SessionProvider>
+    );
 }
 
 export default MyApp;
