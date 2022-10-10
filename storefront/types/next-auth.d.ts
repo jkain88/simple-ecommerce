3c6749ca4580041a336968ab2d49f6d6a71e390b
Martin <martinjosh@vashcorp.com>
2022-10-10 19:02:32 +0800

    Update Session and JWT interface

---
+++ storefront/types/next-auth.d.ts
@@ -0,0 +1,18 @@
+import NextAuth from 'next-auth';
+
+declare module 'next-auth' {
+    /**
+     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
+    interface Session {
+        id: string;
+    }
+}
+
+declare module 'next-auth/jwt' {
+    /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+    interface JWT {
+        /** OpenID ID Token */
+        id: string;
+    }
+}
