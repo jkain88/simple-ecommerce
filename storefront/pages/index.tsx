170bd6975bb3d7bd702c52070e34a04407fbae08
Martin <martinjosh@vashcorp.com>
2022-10-10 18:13:05 +0800

    Update login testing

--- storefront/pages/index.tsx
+++ storefront/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="font-bold text-2xl">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
