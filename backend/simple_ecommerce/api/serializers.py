af024c05ecd07ad7b7add3d79568c341a06e548c
Martin <martinjosh@vashcorp.com>
2022-10-10 19:01:59 +0800

    Add access and refresh token fields

--- backend/simple_ecommerce/api/serializers.py
+++ backend/simple_ecommerce/api/serializers.py
@@ -1,11 +1,14 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from simple_ecommerce.user.serializers import UserSerializer
 
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    access = serializers.CharField(read_only=True)
+    refresh = serializers.CharField(read_only=True)
+
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
