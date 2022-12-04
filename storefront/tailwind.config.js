8df16776899d1b18ed35481d33a90464dc89319b
martinjosh <martinjosh@vashcorp.com>
2022-12-04 22:12:35 +0800

    Update secondary color

--- storefront/tailwind.config.js
+++ storefront/tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#030E4F',
-        secondary: '#F49F1C',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#030E4F',
+                secondary: '#FFFFFF',
+            },
+        },
     },
-  },
-  plugins: [require('@tailwindcss/forms')],
+    plugins: [require('@tailwindcss/forms')],
 };
