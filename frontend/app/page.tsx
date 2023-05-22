edbc9974d1c4c0e0ec67fadac660ab534150efa3
Martin Josh <jkain88@yahoo.com>
2023-05-22 21:57:10 +0800

    Fix hero mobile responsiveness

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,31 +3,10 @@ import Image from 'next/image'
 export default function Home() {
     return (
         <main>
-            <div className='md:min-h-screenz-0 mb-8 flex min-h-screen w-full flex-col items-center justify-center bg-[url("../public/hero.jpg")] bg-cover bg-center bg-no-repeat text-white'>
-                <p className="font-serif text-3xl tracking-wider">
-                    WELCOME TO THE GREENSTORE
-                </p>
-                <h1 className="mt-20 font-serif text-8xl">
-                    Let’s Bring the Spring to
-                    <span className="block text-center">Your Home</span>
-                </h1>
-                <button className="p mt-10 border-solid bg-white px-14 py-5 text-xl font-semibold text-black">
-                    Shop Now
-                </button>
-            </div>
-            <div className="mx-10 mt-20 flex flex-col justify-center gap-10 md:mx-52">
-                <div className="flex w-full flex-col justify-between md:flex-row ">
-                    <p className="text-2xl">New Plants</p>
-                    <button>Shop Now</button>
-                </div>
-                <section>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Assumenda, eum minus omnis nisi illum ea accusantium,
-                    incidunt quibusdam commodi fuga eveniet exercitationem vitae
-                    voluptatum laborum voluptates voluptatibus. Animi, deserunt
-                    magnam.
-                </section>
-            </div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed aut
+            perferendis minima sunt itaque mollitia dignissimos nobis, atque
+            doloremque illum voluptates veniam totam ea vitae pariatur corrupti
+            aliquid eaque nesciunt.
         </main>
     )
 }
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -11,16 +11,39 @@ const Navbar: React.FC = () => {
     console.log(pathname)
 
     return (
-        <nav className="sticky top-0 flex w-full items-center justify-between bg-transparent text-black">
-            <Image src="/logo.svg" width={200} height={150} alt="logo" />
-            <div className="flex gap-14 text-xl">
-                <Link href="/">Home</Link>
-                <Link href="/products">Products</Link>
-                <a href="#">About</a>
-                <a href="#">Contact</a>
-                <a href="#">$0.00</a>
+        <div className="flex min-h-screen w-full flex-col bg-[url('../public/hero.jpg')] bg-no-repeat bg-center bg-cover text-white">
+            <nav className="flex w-full items-center justify-between bg-transparent py-8 px-10">
+                <Image src="/logo.svg" width={200} height={150} alt="logo" />
+                <div className="flex gap-14 text-lg">
+                    <Link href="/" className="hidden lg:block">
+                        Home
+                    </Link>
+                    <Link href="/products" className="hidden lg:block">
+                        Products
+                    </Link>
+                    <Link href="#" className="hidden lg:block">
+                        About
+                    </Link>
+                    <Link href="#" className="hidden lg:block">
+                        Contact
+                    </Link>
+                    <Link href="#">$0.00</Link>
+                </div>
+            </nav>
+            <div className="flex flex-col items-center justify-center flex-grow text-center p-10 md:p-2">
+                <p className="font-serif text-md md:text-3xl tracking-wide">
+                    WELCOME TO THE GREENSTORE
+                </p>
+                <h1 className="mt-5 md:mt-12 font-serif text-4xl md:text-6xl tracking-wider">
+                    Let’s Bring the Spring to
+                    <span className="block text-center">Your Home</span>
+                </h1>
+
+                <button className="p mt-12 border-solid bg-white px-8 md:px-14 py-3 md:py-5 text-md md:text-xl font-semibold text-black">
+                    Shop Now
+                </button>
             </div>
-        </nav>
+        </div>
     )
 }
 
