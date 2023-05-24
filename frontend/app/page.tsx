52a3e44ee233f0d9c4b7afea6ee75ff1b10365d7
Martin Josh <jkain88@yahoo.com>
2023-05-24 22:52:49 +0800

    Add "Our Story" section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -22,7 +22,7 @@ export default async function Home() {
             <section className="mt-14 px-6 md:px-80">
                 <div className="flex justify-between py-8">
                     <p className="font-serif text-4xl">New Plants</p>
-                    <button className="bg-green-700 px-6 py-3 text-sm font-bold text-white">
+                    <button className="bg-lime-700 px-6 py-3 text-sm font-bold text-white">
                         Shop Now
                     </button>
                 </div>
@@ -45,6 +45,40 @@ export default async function Home() {
                     />
                 </div>
             </section>
+
+            <article className="mt-14 flex items-center gap-14">
+                <img
+                    src="/our_story.jpg"
+                    width="0"
+                    height="0"
+                    sizes="100vw"
+                    alt="plants"
+                    className="h-[500px] w-[950px]"
+                />
+
+                <div className="flex w-1/4 flex-col gap-5">
+                    <p className="font-serif text-xl uppercase tracking-widest">
+                        Our story
+                    </p>
+                    <p className="font-serif text-3xl">
+                        For People Who Love Plants
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Aliquid facilis, natus in quam tempora maxime at, dolore
+                        ut possimus nihil velit corporis reprehenderit quo
+                        veniam fugit atque! Praesentium, expedita optio?
+                    </p>
+
+                    <div>
+                        <button className="bg-lime-700 px-6 py-3 text-sm font-bold text-white">
+                            Read More
+                        </button>
+                    </div>
+                </div>
+            </article>
+
+            <section>Reviews</section>
         </main>
     )
 }
