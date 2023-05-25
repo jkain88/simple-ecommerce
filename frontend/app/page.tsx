e81e9a4603cb0e58dd942792c9e670a260db1373
Martin Josh <jkain88@yahoo.com>
2023-05-25 15:12:38 +0800

    Add testimonial section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,10 @@
 import ProductCard from '@/components/ProductCard'
-import Image from 'next/image'
+import { Avatar, AvatarImage } from '@/components/ui/avatar'
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const getProducts = async () => {
-    const response = await fetch('https://fakestoreapi.com/products')
+    const response = await fetch('https://fakestoreapi.com/products?limit=5')
 
     if (!response.ok) {
         throw new Error('Failed to fetch data')
@@ -11,20 +13,33 @@ const getProducts = async () => {
     return response.json()
 }
 
+const getUserTestimonials = async () => {
+    const response = await fetch('https://dummyjson.com/users?limit=3')
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch user testimonials')
+    }
+
+    return response.json()
+}
+
 export default async function Home() {
     const products = await getProducts()
-
+    const { users: userTestimonials } = await getUserTestimonials()
     const product = products[0]
-    console.log(product)
 
     return (
-        <main>
-            <section className="mt-14 px-6 md:px-80">
-                <div className="flex justify-between py-8">
-                    <p className="font-serif text-4xl">New Plants</p>
-                    <button className="bg-lime-700 px-6 py-3 text-sm font-bold text-white">
-                        Shop Now
-                    </button>
+        <main className="">
+            <section className="mt-8 px-6 md:mt-14 md:px-80">
+                <div className="flex flex-col items-center gap-4 py-8 md:flex-row md:justify-between md:gap-0">
+                    <p className="font-serif text-3xl md:text-4xl">
+                        New Plants
+                    </p>
+                    <div>
+                        <button className="bg-primary px-6 py-3 text-sm font-bold text-white">
+                            Shop Now
+                        </button>
+                    </div>
                 </div>
                 <div className="grid grid-cols-2 gap-10 md:grid-cols-3">
                     {products.map((product) => (
@@ -46,7 +61,7 @@ export default async function Home() {
                 </div>
             </section>
 
-            <article className="mt-14 flex items-center gap-14">
+            <article className="mt-14 flex flex-col items-center gap-6 md:flex-row md:gap-24">
                 <img
                     src="/our_story.jpg"
                     width="0"
@@ -56,29 +71,70 @@ export default async function Home() {
                     className="h-[500px] w-[950px]"
                 />
 
-                <div className="flex w-1/4 flex-col gap-5">
-                    <p className="font-serif text-xl uppercase tracking-widest">
+                <div className="flex flex-col gap-5 px-6 md:w-1/4 md:px-0">
+                    <p className="font-serif text-lg uppercase tracking-widest md:text-2xl">
                         Our story
                     </p>
-                    <p className="font-serif text-3xl">
+                    <p className="font-serif text-3xl md:text-4xl">
                         For People Who Love Plants
                     </p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Aliquid facilis, natus in quam tempora maxime at, dolore
-                        ut possimus nihil velit corporis reprehenderit quo
-                        veniam fugit atque! Praesentium, expedita optio?
-                    </p>
+                    <div className="flex flex-col gap-4 text-sm">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Aliquid facilis, natus in quam tempora maxime
+                            at, dolore ut possimus nihil velit corporis
+                            reprehenderit quo veniam fugit atque! Praesentium,
+                            expedita optio?
+                        </p>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Harum animi dolorum quas dolores alias,
+                            voluptatem nostrum itaque repellendus hic ad
+                            cupiditate ipsam veritatis consequuntur reiciendis
+                            ea velit voluptatum earum labore?
+                        </p>
+                    </div>
 
                     <div>
-                        <button className="bg-lime-700 px-6 py-3 text-sm font-bold text-white">
+                        <button className="bg-primary px-6 py-3 text-sm font-bold text-white">
                             Read More
                         </button>
                     </div>
                 </div>
             </article>
 
-            <section>Reviews</section>
+            <section className="mt-20 text-center">
+                <p className="font-serif text-3xl leading-5 underline decoration-primary decoration-1 underline-offset-4">
+                    {' '}
+                    What Our Customers Say
+                </p>
+                <div className="mt-4 divide-x-4 divide-black" />
+
+                <div className="mt-10 grid grid-cols-1 justify-center gap-12 px-10 font-semibold md:grid-cols-3 md:gap-28 md:px-80">
+                    {userTestimonials.map((user) => (
+                        <div className="flex flex-col items-center gap-2 md:gap-4">
+                            <FontAwesomeIcon
+                                icon={faQuoteLeft}
+                                className="mx-auto w-7 text-primary"
+                            />
+                            <p>
+                                Sed odio donec curabitur auctor amet tincidunt
+                                non odio enim felis tincidunt amet morbi egestas
+                                hendrerit.
+                            </p>
+                            <Avatar>
+                                <AvatarImage
+                                    className="w-80"
+                                    src={user.image}
+                                />
+                            </Avatar>
+                            <p className="text-sm">
+                                {user.firstName} {user.lastName}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            </section>
         </main>
     )
 }
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -11,8 +11,8 @@ const Navbar: React.FC = () => {
     console.log(pathname)
 
     return (
-        <div className="flex min-h-screen w-full flex-col bg-[url('../public/hero.jpg')] bg-no-repeat bg-center bg-cover text-white">
-            <nav className="flex w-full items-center justify-between bg-transparent py-8 px-10">
+        <div className="flex min-h-screen w-full flex-col bg-[url('../public/hero.jpg')] bg-cover bg-center bg-no-repeat text-white">
+            <nav className="flex w-full items-center justify-between bg-transparent px-10 py-8">
                 <Image src="/logo.svg" width={200} height={150} alt="logo" />
                 <div className="flex gap-14 text-lg">
                     <Link href="/" className="hidden lg:block">
@@ -30,16 +30,16 @@ const Navbar: React.FC = () => {
                     <Link href="#">$0.00</Link>
                 </div>
             </nav>
-            <div className="flex flex-col items-center justify-center flex-grow text-center p-10 md:p-2">
-                <p className="font-serif text-md md:text-3xl tracking-wide">
+            <div className="flex flex-grow flex-col items-center justify-center p-10 text-center md:p-2">
+                <p className="text-md font-serif tracking-wide md:text-3xl">
                     WELCOME TO THE GREENSTORE
                 </p>
-                <h1 className="mt-5 md:mt-12 font-serif text-4xl md:text-6xl tracking-wider">
+                <h1 className="mt-5 font-serif text-4xl tracking-wider md:mt-12 md:text-6xl">
                     Let’s Bring the Spring to
                     <span className="block text-center">Your Home</span>
                 </h1>
 
-                <button className="p mt-12 border-solid bg-white px-8 md:px-14 py-3 md:py-5 text-md md:text-xl font-semibold text-black">
+                <button className="p text-md mt-12 border-solid bg-white px-8 py-3 font-semibold text-black md:px-14 md:py-5 md:text-xl">
                     Shop Now
                 </button>
             </div>
