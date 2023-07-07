3cbdc3252e7842ef37199d66dcc58491effeb426
Martin Josh Torricer <martinjosh@pop-os.localdomain>
2023-07-07 19:10:21 +0800

    Add sign in fields

--- frontend/app/(auth)/signin/page.tsx
+++ frontend/app/(auth)/signin/page.tsx
@@ -1,4 +1,5 @@
 import { OAuth } from '@/components/auth/oauth'
+import { Button } from '@/components/ui/button'
 import {
     Card,
     CardContent,
@@ -6,11 +7,13 @@ import {
     CardHeader,
     CardTitle,
 } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import React from 'react'
 
 export default function SignInPage() {
     return (
-        <div className=" mx-auto my-auto max-w-lg">
+        <div className=" mx-auto my-auto max-w-2xl xl:w-1/2">
             <Card>
                 <CardHeader>
                     <CardTitle>Sign in</CardTitle>
@@ -27,6 +30,29 @@ export default function SignInPage() {
                         </span>
                         <div className="flex-grow border-t border-gray-500"></div>
                     </div>
+                    <div>
+                        <Label className="text-sm" htmlFor="email">
+                            Email
+                        </Label>
+                        <Input
+                            type="email"
+                            id="email"
+                            placeholder="Email"
+                            className="mt-2"
+                        />
+                    </div>
+                    <div className="mt-4">
+                        <Label className="text-sm" htmlFor="password">
+                            Password
+                        </Label>
+                        <Input
+                            type="password"
+                            id="password"
+                            placeholder="*******"
+                            className="mt-2"
+                        />
+                    </div>
+                    <Button className="mt-4 w-full">Sign in</Button>
                 </CardContent>
             </Card>
         </div>
--- frontend/components/auth/oauth.tsx
+++ frontend/components/auth/oauth.tsx
@@ -7,12 +7,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export function OAuth() {
     return (
-        <div className="flex gap-4">
-            <div className="flex gap-2 rounded-lg border-2 border-solid px-6 py-2 font-bold">
+        <div className="flex justify-between gap-4">
+            <div className="flex grow justify-center gap-2 rounded-lg border-2 border-solid px-6 py-2 font-bold">
                 <FontAwesomeIcon icon={faGoogle} className="w-5 text-black " />
                 <p>Google</p>
             </div>
-            <div className="flex gap-2 rounded-lg border-2 border-solid px-6 py-2 font-bold">
+            <div className="flex grow justify-center gap-2 rounded-lg border-2 border-solid px-6 py-2 font-bold">
                 <FontAwesomeIcon
                     icon={faFacebook}
                     className="w-5 text-black "
