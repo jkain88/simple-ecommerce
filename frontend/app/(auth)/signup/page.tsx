685acfe2adacc733b26d78ba7ee4a7969ba9693c
Martin <jkain88@yahoo.com>
2023-07-08 22:56:59 +0800

    Add verify-email route

--- frontend/app/(auth)/signup/page.tsx
+++ frontend/app/(auth)/signup/page.tsx
@@ -1,13 +1,20 @@
-import { OAuth } from '@/components/auth/oauth';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import Link from 'next/link';
-import React from 'react';
+import { OAuth } from '@/components/auth/oauth'
+import { Button } from '@/components/ui/button'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import Link from 'next/link'
+import React from 'react'
 
 export default function Signup() {
-  return (
+    return (
         <div className="z-20 max-w-2xl sm:flex-auto xl:w-1/2">
             <Card>
                 <CardHeader>
@@ -47,17 +54,23 @@ export default function Signup() {
                             className="mt-2"
                         />
                     </div>
-                    <Button className="mt-4 w-full">Continue</Button>
+                    <Link href="/signup/verify-email">
+                        <Button className="mt-4 w-full">Continue</Button>
+                    </Link>
                 </CardContent>
                 <CardFooter>
                     <div className="flex w-full items-center justify-between gap-2 ">
                         <Link href="/signin">
-                            <span className="mr-1 hidden text-sm lg:inline-block">Already have an account?</span>
-                            <span className="text-sm font-semibold">Sign in</span>
+                            <span className="mr-1 hidden text-sm lg:inline-block">
+                                Already have an account?
+                            </span>
+                            <span className="text-sm font-semibold">
+                                Sign in
+                            </span>
                         </Link>
                     </div>
                 </CardFooter>
             </Card>
         </div>
-  );
-};
+    )
+}
---
+++ frontend/app/(auth)/signup/verify-email/page.tsx
@@ -0,0 +1,41 @@
+import { Button } from '@/components/ui/button'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import React from 'react'
+
+export default function VerifyEmailPage() {
+    return (
+        <div className="z-20 max-w-2xl sm:flex-auto xl:w-1/2">
+            <Card>
+                <CardHeader>
+                    <CardTitle>Verify email</CardTitle>
+                    <CardDescription>
+                        Verify your email address to continue your sign up
+                        process
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div>
+                        <Label className="text-sm" htmlFor="verification-code">
+                            Verification Code
+                        </Label>
+                        <Input
+                            type="text"
+                            id="verification-code"
+                            placeholder="101010"
+                            className="mt-2"
+                        />
+                    </div>
+                    <Button className="mt-4 w-full">Verify</Button>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
