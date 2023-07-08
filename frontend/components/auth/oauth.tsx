c284111499b53785fbc2029874eff14db52b5d55
Martin <jkain88@yahoo.com>
2023-07-08 22:47:52 +0800

    Change to buttons

--- frontend/components/auth/oauth.tsx
+++ frontend/components/auth/oauth.tsx
@@ -8,17 +8,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export function OAuth() {
     return (
         <div className="flex flex-col justify-between gap-4 sm:flex-row">
-            <div className="flex grow justify-center gap-2 rounded-lg border-2 border-solid px-6 py-2 font-bold">
+            <button className="flex grow justify-center gap-2 rounded-lg border-2 border-solid px-6 py-2 font-bold">
                 <FontAwesomeIcon icon={faGoogle} className="w-5 text-black " />
                 <p>Google</p>
-            </div>
-            <div className="flex grow justify-center gap-2 rounded-lg border-2 border-solid px-6 py-2 font-bold">
+            </button>
+            <button className="flex grow justify-center gap-2 rounded-lg border-2 border-solid px-6 py-2 font-bold">
                 <FontAwesomeIcon
                     icon={faFacebook}
                     className="w-5 text-black "
                 />
                 <p>Facebook</p>
-            </div>
+            </button>
         </div>
     )
 }
