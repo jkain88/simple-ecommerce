fd904cedce34206f7a7b5f53839927a54c4b8d6e
Martin Josh <jkain88@yahoo.com>
2023-07-10 23:29:42 +0800

    Clean up

--- frontend/components/ProductPageContainer.tsx
+++ frontend/components/ProductPageContainer.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
 interface Props {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
 const ProductPageContainer: React.FC<Props> = ({ children }) => {
-    return (
-        <div className="min-h-screen bg-neutral-100 px-10 py-20">
-            <div className="max-w-screen-xl mx-auto flex flex-col gap-10 bg-white py-5 lg:py-20 px-10 lg:px-20 shadow-2xl">
-                {children}
-            </div>
-        </div>
-    )
+  return (
+    <div className="min-h-screen bg-neutral-100 px-10 py-20">
+      <div className="mx-auto flex max-w-screen-xl flex-col gap-10 bg-white px-10 py-5 shadow-2xl lg:px-20 lg:py-20">
+        {children}
+      </div>
+    </div>
+  )
 }
 
 export default ProductPageContainer
