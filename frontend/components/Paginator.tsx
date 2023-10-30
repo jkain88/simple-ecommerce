d51a373ddba80eb005753f7382b1174d5058fcb5
Martin Josh <jkain88@yahoo.com>
2023-10-30 23:05:03 +0800

    prettier

--- frontend/components/Paginator.tsx
+++ frontend/components/Paginator.tsx
@@ -1,45 +1,47 @@
-import classNames from 'classnames';
-import React from 'react';
+import classNames from 'classnames'
+import React from 'react'
 
 const Paginator: React.FC = () => {
   const pageData = [
     {
-        page: 1,
-        isActive: true
+      page: 1,
+      isActive: true,
     },
     {
-        page: 2,
-        isActive: false
+      page: 2,
+      isActive: false,
     },
     {
-        page: 3,
-        isActive: false
+      page: 3,
+      isActive: false,
     },
     {
-        page: 4,
-        isActive: false
+      page: 4,
+      isActive: false,
     },
     {
-        page: 5,
-        isActive: false
+      page: 5,
+      isActive: false,
     },
-  ]  
+  ]
   return (
-    <div className="flex gap-2">
-        {
-            pageData.map((data) => (
-                <div className={classNames("cursor-pointer transition duration-500 hover:bg-black hover:text-white border-black border-solid border-2 py-2 px-4 text-sm", 
-                    {
-                        'bg-black' : data.isActive,
-                        'text-white': data.isActive
-                    }
-                )}>
-                    {data.page}
-                </div>
-            ))
-        }
+    <div className="mt-10 flex gap-2">
+      {pageData.map((data) => (
+        <div
+          key={data.page}
+          className={classNames(
+            'cursor-pointer border-2 border-solid border-black px-4 py-2 text-sm duration-500 transition hover:bg-black hover:text-white',
+            {
+              'bg-black': data.isActive,
+              'text-white': data.isActive,
+            }
+          )}
+        >
+          {data.page}
+        </div>
+      ))}
     </div>
-  );
-};
+  )
+}
 
-export default Paginator;
+export default Paginator
