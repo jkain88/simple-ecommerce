f0a4daa3beb20760dbcc132f25fa9758a164b5f5
Martin <martinjosh@vashcorp.com>
2023-11-03 23:59:29 +0800

    Update test categorie sdata

--- frontend/constants/testData.tsx
+++ frontend/constants/testData.tsx
@@ -1,8 +1,32 @@
 export const categories = [
-  "What's new",
-  'Best sellers',
-  'Suits',
-  'Dresses',
-  'Swimsuit',
-  'Accessories',
+  {
+    id: 1,
+    name: "What's new",
+    slug: 'whats-new',
+  },
+  {
+    id: 2,
+    name: 'Best sellers',
+    slug: 'best-sellers',
+  },
+  {
+    id: 3,
+    name: 'Suits',
+    slug: 'suits',
+  },
+  {
+    id: 4,
+    name: 'Dresses',
+    slug: 'dresses',
+  },
+  {
+    id: 5,
+    name: 'Swimsuit',
+    slug: 'swimsuit',
+  },
+  {
+    id: 6,
+    name: 'Accessories',
+    slug: 'accessories',
+  },
 ]
