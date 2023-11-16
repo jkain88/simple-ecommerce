deed3d8756f2660419e945adc06e242d7f8ee486
Martin Josh <jkain88@yahoo.com>
2023-11-16 23:35:53 +0800

    Show address data

--- frontend/app/(client-dashboard)/account/addresses/page.tsx
+++ frontend/app/(client-dashboard)/account/addresses/page.tsx
@@ -1,8 +1,30 @@
+import { addresses } from '@/constants/testData'
+
 export default function Addresses() {
   return (
-    <div>
-      <p className="text-3xl font-bold">Addresses</p>
-      <div className="divide-y-8 divide-black" />
+    <div className="divide-y-1">
+      <p className="pb-3 text-3xl font-bold">My Addresses</p>
+      <div className="pt-5">
+        <p className="text-2xl font-semibold">Address</p>
+        <div className="flex flex-col gap-7 divide-y-2">
+          {addresses.map((address) => (
+            <div key={address.id} className="flex flex-col gap-2 pt-7">
+              <div className="flex gap-2 divide-x-2 divide-black">
+                <p className="text-lg font-bold">{address.name}</p>
+                <p className="pl-2 text-gray-400">{address.contactNumber}</p>
+              </div>
+              <p>{address.address}</p>
+              {address.isDefault && (
+                <div className="flex-shrink-0">
+                  <div className="inline-block border-2 border-black p-1 ">
+                    <p className="text-xs font-semibold uppercase">Default</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
