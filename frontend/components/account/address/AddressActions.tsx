1ac67da0dc0cf27c5be67826c3d4ba55a9bd1051
Martin <martinjosh@vashcorp.com>
2023-11-18 00:32:07 +0800

    Update address action navigation

--- frontend/components/account/address/AddressActions.tsx
+++ frontend/components/account/address/AddressActions.tsx
@@ -1,18 +1,31 @@
 'use client'
 
-import { Button } from '@nextui-org/react'
+// import { Button } from '@nextui-org/react'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const AddressActions: React.FC = () => {
   return (
     <div className="mt-2 flex flex-col gap-1">
-      <Button size="sm" className="font-semibold">
-        Edit
-      </Button>
-      <Button size="sm" className="font-semibold">
+      <Link href="/account/addresses/1" className="flex">
+        <Button
+          size="sm"
+          className="w-full bg-gray-200 px-10 text-xs font-semibold text-black hover:bg-gray-500"
+        >
+          Edit
+        </Button>
+      </Link>
+      <Button
+        size="sm"
+        className="w-full bg-gray-200 px-10 text-xs font-semibold text-black hover:bg-gray-500"
+      >
         Delete
       </Button>
-      <Button size="sm" className="font-semibold">
+      <Button
+        size="sm"
+        className="bg-gray-200 px-10 text-xs font-semibold text-black hover:bg-gray-500"
+      >
         Set as Default
       </Button>
     </div>
