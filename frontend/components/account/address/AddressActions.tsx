1bce269a1777bf20c8784358f786de4139b0fedf
Martin <martinjosh@vashcorp.com>
2023-11-18 22:42:49 +0800

    Add on address delete modal

--- frontend/components/account/address/AddressActions.tsx
+++ frontend/components/account/address/AddressActions.tsx
@@ -1,7 +1,14 @@
 'use client'
 
-// import { Button } from '@nextui-org/react'
 import { Button } from '@/components/ui/button'
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from '@nextui-org/react'
 import Link from 'next/link'
 import React from 'react'
 
@@ -10,6 +17,11 @@ type Props = {
 }
 
 const AddressActions: React.FC<Props> = ({ id }) => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure()
+  const onSubmitDelete = (onClose: () => void) => {
+    console.log('SUBMIT')
+    onClose()
+  }
   return (
     <div className="mt-2 flex flex-col gap-1">
       <Link href={`/account/addresses/${id}`} className="flex">
@@ -23,9 +35,27 @@ const AddressActions: React.FC<Props> = ({ id }) => {
       <Button
         size="sm"
         className="w-full bg-gray-200 px-10 text-sm font-semibold text-black hover:bg-gray-500"
+        onClick={onOpen}
       >
         Delete
       </Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Delete Address
+              </ModalHeader>
+              <ModalBody>
+                <p>Are you sure you want to delete this address?</p>
+              </ModalBody>
+              <ModalFooter>
+                <Button onClick={() => onSubmitDelete(onClose)}>Submit</Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
       <Button
         size="sm"
         className="bg-gray-200 px-10 text-sm font-semibold text-black hover:bg-gray-500"
