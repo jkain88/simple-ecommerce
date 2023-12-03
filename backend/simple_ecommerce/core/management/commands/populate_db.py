db69dcb2a4be3f20ddda9bdbc7c9106db428e0fb
Martin <martinjosh@vashcorp.com>
2023-12-03 13:10:03 +0800

    Update populate db create products

--- backend/simple_ecommerce/core/management/commands/populate_db.py
+++ backend/simple_ecommerce/core/management/commands/populate_db.py
@@ -1,29 +1,72 @@
+from decimal import Decimal
 import os
+import requests
+
 from django.core.files import File
 from django.core.management.base import BaseCommand
 
-from simple_ecommerce.product.models import Category, Product, ProductVariant, ProductImage
+from simple_ecommerce.product.models import (
+    Category,
+    Product,
+    ProductVariant,
+    ProductImage,
+)
 
 
 # Get the directory of the current script
 script_dir = os.path.dirname(__file__)
 
-# Construct the path to the image file
+
+def create_zalora_products(category_name):
+    zalora_category = {
+        "Men Clothes": {"id": "96", "segment": "men"},
+        "Women Clothes": {"id": "106", "segment": "women"},
+        "Sports": {"id": "258", "segment": "women"},
+        "Electronics": {"id": "11031", "segment": "home"},
+    }
+    API_URL = f"https://api.zalora.com.ph/v1/dynproducts/datajet/list?abtest=djAbTest_True%7Cuuid%3A82838788-cd7c-49bb-8ead-c316db09e955&categoryId={zalora_category[category_name]['id']}&fullFacetCategory=true&image_format=webp&image_quality=70&limit=10&offset=0&segment={zalora_category[category_name]['segment']}&shop=m"
+    headers = {
+        "Accept": "application/json",
+    }
+
+    response = requests.get(API_URL, headers=headers).json()
+    products = response["data"]["Products"]
+
+    for product in products:
+        price = Decimal(product["Price"].replace("Php ", "").replace(",", ""))
+        name = " ".join(product["Name"].split(" ")[:5])
+        db_product, _ = Product.objects.get_or_create(
+            name=name, price=price, sku=product["ConfigSku"], quantity=1000
+        )
+        print("PRODUCT", db_product)
+
+        for index, image_url in enumerate(product["ImageList"]):
+            image_data = requests.get(image_url).content
+            image_name = "-".join(name.split(" ")).lower() + f"-{index}.webp"
+            print(image_name)
+            image_path = f"/app/simple_ecommerce/static/images/products/{image_name}"
+
+            if not os.path.exists(image_path):
+                os.makedirs(os.path.dirname(image_path), exist_ok=True)
+                with open(
+                    image_path,
+                    "wb",
+                ) as handler:
+                    handler.write(image_data)
+
+            product_image = {"alt": name, "product": db_product}
+            with open(image_path, "rb") as f:
+                product_image["image"] = File(f, name=image_name)
+                print(f"Creating {image_name} product image")
+                ProductImage.objects.get_or_create(**product_image)
+
 
 def populate_categories():
     categories = [
-        {
-            "name": "Men Clothes"
-        },
-        {
-            "name": "Women Clothes"
-        },
-        {
-            "name": "Sports"
-        },
-        {
-            "name": "Electronics"
-        },
+        {"name": "Men Clothes"},
+        {"name": "Women Clothes"},
+        {"name": "Sports"},
+        {"name": "Electronics"},
     ]
 
     for category in categories:
@@ -31,51 +74,13 @@ def populate_categories():
         Category.objects.get_or_create(**category)
 
 
-def populate_products():
-    products = [
-        {
-            "name": "MANGO Man",
-            "category": Category.objects.get(name="Men Clothes"),
-
-        }
-    ]
-
-    for product in products:
-        print(f"Creating {product['name']} product")
-        Product.objects.get_or_create(**product)
-
-    product_images = [
-        {
-            "image_path": "/app/simple_ecommerce/static/images/products/mango-man-1.webp",
-            "product": Product.objects.get(name="MANGO Man"),
-            "alt": "MANGO Man"
-        },
-        {
-            "image_path": "/app/simple_ecommerce/static/images/products/mango-man-2.webp",
-            "product": Product.objects.get(name="MANGO Man"),
-            "alt": "MANGO Man"
-        },
-        {
-            "image_path": "/app/simple_ecommerce/static/images/products/mango-man-3.webp",
-            "product": Product.objects.get(name="MANGO Man"),
-            "alt": "MANGO Man"
-        },
-    ]
-
-    print(script_dir)
-    for product_image in product_images:
-        path = product_image.pop("image_path")
-        with open(path, "rb") as f:
-            image_name = path.split("/")[-1]
-            product_image["image"] = File(f, name=image_name)
-            print(f"Creating {image_name} product image")
-            ProductImage.objects.get_or_create(**product_image)
-
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self.stdout.write('Populating database...')
+        self.stdout.write("Populating database...")
 
         populate_categories()
-        populate_products()
+        categories = Category.objects.all()
+        for category in categories:
+            create_zalora_products(category.name)
 
-        self.stdout.write(self.style.SUCCESS('Database populated!'))
+        self.stdout.write(self.style.SUCCESS("Database populated!"))
