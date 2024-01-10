dbeda6e561e47640b448cee93021d20460ea5bb4
Martin Josh <martinjosh@vashcorp.com>
2024-01-10 21:29:00 +0800

    Add custom pagination class

--- backend/simple_ecommerce/settings/base.py
+++ backend/simple_ecommerce/settings/base.py
@@ -133,8 +133,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 100,
+    "DEFAULT_PAGINATION_CLASS": "simple_ecommerce.core.pagination.CustomPagination",
 }
 
 # Prices currency
