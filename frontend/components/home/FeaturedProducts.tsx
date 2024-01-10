c32ca3d4717170d86a9b57cfbe41cdf1c702f7e6
Martin Josh <martinjosh@vashcorp.com>
2024-01-10 22:03:14 +0800

    Update featured products query key

--- frontend/components/home/FeaturedProducts.tsx
+++ frontend/components/home/FeaturedProducts.tsx
@@ -8,7 +8,7 @@ import { Api, Product } from '@/lib/Api'
 
 const FeaturedProducts: React.FC = () => {
   const { data: products, isLoading } = useQuery({
-    queryKey: ['products'],
+    queryKey: ['featured-products'],
     queryFn: () => {
       const api = new Api()
       const response = api.products.productsList({ is_featured: 'true' })
@@ -21,16 +21,17 @@ const FeaturedProducts: React.FC = () => {
       <h1 className="text-center text-3xl">Featured Products</h1>
       <p className="mt-2 text-center text-sm">Handpicked Just for You!</p>
       <div className="mt-5 grid max-w-max grid-cols-2 gap-10 px-10 md:grid-cols-4">
-        {!isLoading && products!.data.results.map((product: Product) => (
-          <Link href={`/products/${product.id}`} key={product.id}>
-            <ProductCard
-              key={product.id}
-              price={product.price}
-              image={product.images[0].image}
-              name={product.name}
-            />
-          </Link>
-        ))}
+        {!isLoading &&
+          products!.data.results.map((product: Product) => (
+            <Link href={`/products/${product.id}`} key={product.id}>
+              <ProductCard
+                key={product.id}
+                price={product.price}
+                image={product.images[0].image}
+                name={product.name}
+              />
+            </Link>
+          ))}
       </div>
     </section>
   )
