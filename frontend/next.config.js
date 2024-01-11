f74cd9ef4875170d798ecfc509138b9ee1ef4238
Martin Josh <martinjosh@vashcorp.com>
2024-01-11 16:54:12 +0800

    Update next images domains

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'fakestoreapi.com',
-                port: '',
-            },
-        ],
-    },
+  images: {
+    domains: ['fakestoreapi.com', 'localhost'],
+  },
 }
 
 module.exports = nextConfig
