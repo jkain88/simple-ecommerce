72cc75180dcf6f936c34c2bc7ea50c2f2181cf33
Martin Josh <martinjosh@vashcorp.com>
2024-01-11 14:57:21 +0800

    Implement new arrival products infinite query

--- frontend/components/home/NewArrivals.tsx
+++ frontend/components/home/NewArrivals.tsx
@@ -4,18 +4,35 @@ import Link from 'next/link'
 import React from 'react'
 import ProductCard from '../ProductCard'
 import { Button } from '@nextui-org/react'
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 import { Api, Product } from '@/lib/Api'
+import { extractPageFromUrl } from '@/lib/utils'
+
+const getProducts = async ({ pageParam = 1 }) => {
+  const api = new Api()
+  const response = await api.products.productsList({
+    page_size: 10,
+    page: pageParam,
+  })
+  return response.data
+}
 
 const NewArrivals: React.FC = () => {
-  const { data: products, isLoading } = useQuery({
+  const {
+    data: products,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+  } = useInfiniteQuery({
     queryKey: ['new-arrival-products'],
-    queryFn: () => {
-      const api = new Api()
-      const response = api.products.productsList({ page_size: 10 })
-      return response
+    queryFn: getProducts,
+    getNextPageParam: (lastPage) => {
+      const nextPage = extractPageFromUrl(lastPage.next)
+      return nextPage
     },
+    initialPageParam: 1,
   })
+  console.log('HAS NEXT PAGE', hasNextPage)
   return (
     <section className="mb-8 flex flex-col items-center px-10">
       <h1 className="text-center text-3xl">New Arrivals</h1>
@@ -24,18 +41,23 @@ const NewArrivals: React.FC = () => {
       </p>
       <div className="mt-5 grid max-w-max grid-cols-2 gap-10 px-10 md:grid-cols-4">
         {!isLoading &&
-          products!.data.results.map((product: Product) => (
-            <Link href={`/products/${product.id}`} key={product.id}>
-              <ProductCard
-                key={product.id}
-                price={product.price}
-                image={product.images[0].image}
-                name={product.name}
-              />
-            </Link>
-          ))}
+          products!.pages.map((page, i) =>
+            page.results.map((product: Product) => (
+              <Link href={`/products/${product.id}`} key={product.id}>
+                <ProductCard
+                  key={product.id}
+                  price={product.price}
+                  image={product.images[0].image}
+                  name={product.name}
+                />
+              </Link>
+            ))
+          )}
       </div>
-      <Button className="mb-20 mt-10 bg-black px-8 text-white">
+      <Button
+        className="mb-20 mt-10 bg-black px-8 text-white"
+        onClick={() => fetchNextPage()}
+      >
         View More
       </Button>
     </section>
--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const extractPageFromUrl = (url: string | null | undefined) => {
+  if (!url) return null
+
+  const urlObj = new URL(url)
+  const page = urlObj.searchParams.get('page')
+  return page ? parseInt(page) : null
+}
