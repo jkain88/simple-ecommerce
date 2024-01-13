d389effab5fdad4f7999cfe2f62a75c2b7480c47
Martin <martinjosh@vashcorp.com>
2024-01-13 23:11:05 +0800

    Delete users on prune

--- backend/simple_ecommerce/core/management/commands/prune_db.py
+++ backend/simple_ecommerce/core/management/commands/prune_db.py
@@ -1,9 +1,17 @@
 from django.core.management.base import BaseCommand
 
-from simple_ecommerce.product.models import Category, Product, ProductVariant, ProductImage
+from simple_ecommerce.product.models import (
+    Category,
+    Product,
+    ProductVariant,
+    ProductImage,
+)
+from simple_ecommerce.user.models import User
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        User.objects.all().delete()
         Product.objects.all().delete()
         ProductImage.objects.all().delete()
         Category.objects.all().delete()
