ac7cd643584a1d51ee66a21de1d32cda27b77d81
Martin <martinjosh@vashcorp.com>
2024-01-13 23:29:34 +0800

    Update user sex choices

--- backend/simple_ecommerce/user/choices.py
+++ backend/simple_ecommerce/user/choices.py
@@ -2,17 +2,12 @@ class AddressType:
     BILLING = "billing"
     SHIPPING = "shipping"
 
-    CHOICES = [
-        (BILLING, "Billing"),
-        (SHIPPING, "Shipping")
-    ]
+    CHOICES = [(BILLING, "Billing"), (SHIPPING, "Shipping")]
 
 
 class Sex:
     MALE = "male"
     FEMALE = "female"
+    OTHERS = "others"
 
-    CHOICES = [
-        (MALE, "Male"),
-        (FEMALE, "Female")
-    ]
+    CHOICES = [(MALE, "Male"), (FEMALE, "Female"), (OTHERS, "others")]
