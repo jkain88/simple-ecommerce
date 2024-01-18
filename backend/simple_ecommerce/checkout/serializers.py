f5cd5c7a9c87b67c565bff9fbeeaae4b9a03fcfa
Martin Josh <martinjosh@vashcorp.com>
2024-01-18 13:59:56 +0800

    Update checkout create

--- backend/simple_ecommerce/checkout/serializers.py
+++ backend/simple_ecommerce/checkout/serializers.py
@@ -1,53 +1,54 @@
 from rest_framework import serializers
 
-from .models import (
-    Checkout,
-    CheckoutLine
-)
+from .models import Checkout, CheckoutLine
 from simple_ecommerce.user.serializers import (
-    AddressSerializer, 
+    AddressSerializer,
 )
 from simple_ecommerce.product.serializers import (
     ProductSerializer,
-    ProductVariantSerializer
+    ProductVariantSerializer,
 )
 
 
 class CheckoutLineSerializer(serializers.ModelSerializer):
-    product_detail = ProductSerializer(read_only=True, source='product')
-    product_variant_detail = ProductVariantSerializer(read_only=True, source='product_variant')
+    product_detail = ProductSerializer(read_only=True, source="product")
+    product_variant_detail = ProductVariantSerializer(
+        read_only=True, source="product_variant"
+    )
 
     class Meta:
         model = CheckoutLine
         fields = [
-            'id',
-            'amount',
-            'checkout',
-            'product',
-            'product_detail',
-            'product_variant',
-            'product_variant_detail',
-            'quantity'
+            "id",
+            "amount",
+            "checkout",
+            "product",
+            "product_detail",
+            "product_variant",
+            "product_variant_detail",
+            "quantity",
         ]
-        read_only_fields = ['amount']
+        read_only_fields = ["amount"]
 
 
 class CheckoutSerializer(serializers.ModelSerializer):
-    billing_address_detail = AddressSerializer(read_only=True, source='billing_address')
-    shipping_address_detail = AddressSerializer(read_only=True, source='shipping_address')
+    billing_address_detail = AddressSerializer(read_only=True, source="billing_address")
+    shipping_address_detail = AddressSerializer(
+        read_only=True, source="shipping_address"
+    )
     lines = CheckoutLineSerializer(many=True, read_only=True)
 
     class Meta:
         model = Checkout
         fields = [
-            'id',
-            'billing_address',
-            'billing_address_detail',
-            'lines',
-            'shipping_address',
-            'shipping_address_detail',
-            'total_amount',
-            'user'
+            "id",
+            "billing_address",
+            "billing_address_detail",
+            "lines",
+            "shipping_address",
+            "shipping_address_detail",
+            "total_amount",
+            "user",
         ]
 
 
--- backend/simple_ecommerce/checkout/views.py
+++ backend/simple_ecommerce/checkout/views.py
@@ -1,22 +1,20 @@
 from django.db import transaction
 from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import generics, mixins, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Checkout, CheckoutLine
 from .serializers import (
-    CheckoutSerializer, 
-    CheckoutCompleteSerializer, 
-    CheckoutLineSerializer
-    )
+    CheckoutSerializer,
+    CheckoutCompleteSerializer,
+    CheckoutLineSerializer,
+)
 from .utils import (
     validate_line_input,
 )
-from simple_ecommerce.order.models import (
-    Order, 
-    OrderLine
-)
+from simple_ecommerce.order.models import Order, OrderLine
 from simple_ecommerce.order.serializers import OrderSerializer
 
 
@@ -24,6 +22,17 @@ class CheckoutCreate(generics.CreateAPIView):
     queryset = Checkout.objects.all()
     serializer_class = CheckoutSerializer
     permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        try:
+            checkout = user.checkout
+            serializer = self.get_serializer(checkout)
+            return Response(serializer.data)
+        except ObjectDoesNotExist:
+            pass
+
+        serializer.save(user=user)
 
 
 class CheckoutUpdate(generics.UpdateAPIView):
@@ -44,16 +53,25 @@ class CheckoutDetail(generics.RetrieveAPIView):
 class CheckoutLineCreate(generics.CreateAPIView):
     serializer_class = CheckoutLineSerializer
     permission_classes = [IsAuthenticated]
-    
+
     def perform_create(self, serializer):
         validate_line_input(serializer.validated_data)
         serializer.save()
 
+    # def create(self, request, *args, **kwargs):
+    #     user = self.request.user
+    #     serializer = self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     data = serializer.validated_data
+    #     print("USER", user)
+
+    #     validate_line_input(data)
+    #     return self.create(request, *args, **kwargs)
+
 
 class CheckoutLineDetail(
-    mixins.UpdateModelMixin, 
-    mixins.DestroyModelMixin, 
-    generics.GenericAPIView):
+    mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView
+):
     queryset = CheckoutLine.objects.all()
     serializer_class = CheckoutLineSerializer
     permission_classes = [IsAuthenticated]
@@ -70,8 +88,9 @@ class CheckoutLineDetail(
 
 class CheckoutComplete(generics.GenericAPIView):
     """
-        Create order, order line and delete checkout
+    Create order, order line and delete checkout
     """
+
     serializer_class = CheckoutCompleteSerializer
     permission_classes = [IsAuthenticated]
 
@@ -80,13 +99,13 @@ class CheckoutComplete(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         validated_data = serializer.validated_data
 
-        checkout = get_object_or_404(Checkout, pk=validated_data['checkout'])
+        checkout = get_object_or_404(Checkout, pk=validated_data["checkout"])
         with transaction.atomic():
             order = Order.objects.create(
                 billing_address=checkout.billing_address,
                 shipping_address=checkout.shipping_address,
                 total_amount=checkout.total_amount,
-                user=checkout.user
+                user=checkout.user,
             )
             for checkout_line in checkout.lines.all():
                 OrderLine.objects.create(
@@ -94,7 +113,7 @@ class CheckoutComplete(generics.GenericAPIView):
                     order=order,
                     product=checkout_line.product,
                     product_variant=checkout_line.product_variant,
-                    quantity=checkout_line.quantity
+                    quantity=checkout_line.quantity,
                 )
             checkout.delete()
         return Response(OrderSerializer(order).data, status=status.HTTP_201_CREATED)
