a43989f6065a6e6adb5868b28b52f5993f9dfd38
Martin Josh <martinjosh@vashcorp.com>
2024-01-20 00:18:34 +0800

    Implement categories navigation

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -4,12 +4,22 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 import { ShoppingCart } from 'lucide-react'
-import { Badge } from '@nextui-org/react'
+import { Badge, Input } from '@nextui-org/react'
 import { useQuery } from '@tanstack/react-query'
 import { Api, Category } from '@/lib/Api'
 import { signOut, useSession } from 'next-auth/react'
 import { useUserStore } from '@/store/user'
 import { useCheckoutStore } from '@/store/checkout'
+import { SearchIcon } from '@/icons/SearchIcon'
+import {
+  ListItem,
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from './ui/navigation-menu'
 
 const Navbar: React.FC = () => {
   const router = useRouter()
@@ -37,21 +47,51 @@ const Navbar: React.FC = () => {
 
   return (
     <div className="flex items-center justify-around px-20 py-6 2xl:px-40">
-      <a href="/">
-        <Image src="/black-logo.svg" alt="logo" width={120} height={100} />
-      </a>
+      <div className="flex items-center gap-10">
+        <a href="/">
+          <Image src="/black-logo.svg" alt="logo" width={120} height={100} />
+        </a>
 
-      <div className="flex gap-8 text-sm">
-        {!isLoading &&
-          categories!.data.results.map((category: Category) => (
-            <a
-              href={`/categories/${category.slug}`}
-              key={category.id}
-              className="font-semibold uppercase hover:text-gray-400"
-            >
-              {category.name}
-            </a>
-          ))}
+        <div className="hidden lg:block">
+          <Input
+            size="md"
+            radius="sm"
+            className="w-40 lg:w-80"
+            endContent={
+              <SearchIcon className="pointer-events-none  mb-0.5 flex-shrink-0 text-black dark:text-white/90" />
+            }
+            placeholder="Type to search..."
+          />
+        </div>
+        <div className="hidden gap-8 text-sm lg:flex">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className="text-base font-semibold">
+                  Categories
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  {!isLoading &&
+                    categories!.data.results.map((category: Category) => (
+                      <ul key={category.id}>
+                        <ListItem
+                          className="z-50 px-8 hover:bg-slate-100"
+                          href={`/categories/${category.slug}`}
+                        >
+                          <p className="font-semibold">{category.name}</p>
+                          {/* <a
+                          href={`/categories/${category.slug}`}
+                          className="font-semibold uppercase hover:text-gray-400"
+                        >
+                        </a> */}
+                        </ListItem>
+                      </ul>
+                    ))}
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
       </div>
 
       <div className="flex items-center gap-14 font-bold">
