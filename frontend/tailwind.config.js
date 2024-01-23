fc4aaa9043ad919d7be66a93122310182273cbe4
Martin Josh <martinjosh@vashcorp.com>
2024-01-23 23:39:09 +0800

    Add tailwind transition config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -38,6 +38,10 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      transitionProperty: {
+        height: 'height',
+        spacing: 'margin, padding',
+      },
     },
   },
   darkMode: 'class',
