5569610c6d0c2961cc4b78bc5be9c99e1dee4b19
Martin Josh <martinjosh@vashcorp.com>
2024-01-24 22:14:15 +0800

    Update checkout shipping address

--- frontend/app/checkout/page.tsx
+++ frontend/app/checkout/page.tsx
@@ -1,11 +1,24 @@
+'use client'
+
 import CheckoutLine from '@/components/checkout/CheckoutLine'
 import CheckoutPaymentMethods from '@/components/checkout/CheckoutPaymentMethods'
 import CheckoutSummary from '@/components/checkout/CheckoutSummary'
-import { addresses } from '@/constants/testData'
+import AddressDetailForm from '@/components/forms/AddressDetailForm'
+import { useCheckoutStore } from '@/store/checkout'
+import autoAnimate from '@formkit/auto-animate'
+import { useAnimate } from 'framer-motion'
+import { useEffect, useRef, useState } from 'react'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 export default function Checkout() {
-  const address = addresses.find((address) => address.isDefault)
+  const [isFormVisible, setIsFormVisible] = useState(false)
+  const checkout = useCheckoutStore((state) => state.checkout)
+  const [parent] = useAutoAnimate({
+    duration: 150,
+    easing: 'linear',
+  })
 
+  console.log('ADDRESS', checkout?.shipping_address_detail)
   return (
     <div className="flex justify-center gap-4 bg-gray-100 py-10">
       <div className="w-full max-w-3xl ">
@@ -13,24 +26,42 @@ export default function Checkout() {
           <div>
             <div className="flex justify-between bg-gray-50 px-4 py-2  font-bold">
               <p className="text-lg">Shipping Address</p>
-              <p>Edit</p>
+              <p onClick={() => setIsFormVisible((prev) => !prev)}>Edit</p>
             </div>
-
-            <div className="px-4 py-2">
-              <div className="flex gap-4">
-                <p>{address?.fullName}</p>
-                <p>{address?.contactNumber}</p>
-              </div>
-              <div className="flex items-center gap-4">
-                <div className="my-2 inline-block rounded-lg bg-black px-3 py-1 capitalize text-white">
-                  {address?.deliveryLabel}
+            {checkout?.shipping_address_detail !== undefined && (
+              <>
+                <div className="px-4 py-2">
+                  {isFormVisible ? (
+                    <div className="px-4 pb-4" ref={parent}>
+                      <AddressDetailForm
+                        address={checkout?.shipping_address_detail}
+                        type="update"
+                      />
+                    </div>
+                  ) : (
+                    <div ref={parent}>
+                      <div className="flex gap-4">
+                        {/* <p>{address?.}</p> */}
+                        <p>
+                          {checkout?.shipping_address_detail?.contact_number}
+                        </p>
+                      </div>
+                      <div className="flex items-center gap-4">
+                        <div className="my-2 inline-block rounded-lg bg-black px-3 py-1 capitalize text-white">
+                          {checkout?.shipping_address_detail?.delivery_label}
+                        </div>
+                        <p>
+                          {checkout?.shipping_address_detail?.street},{' '}
+                          {checkout?.shipping_address_detail?.city_area},{' '}
+                          {checkout?.shipping_address_detail?.city},{' '}
+                          {checkout?.shipping_address_detail?.province}
+                        </p>
+                      </div>
+                    </div>
+                  )}
                 </div>
-                <p>
-                  {address?.street}, {address?.barangay}, {address?.city},{' '}
-                  {address?.province}
-                </p>
-              </div>
-            </div>
+              </>
+            )}
           </div>
         </div>
         <CheckoutLine />
