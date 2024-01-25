e3a3aa87c716f52733981d6191790003c5828ce7
Martin Josh <martinjosh@vashcorp.com>
2024-01-25 19:48:15 +0800

    Checkout page fetch checkout lines

--- frontend/components/checkout/CheckoutLine.tsx
+++ frontend/components/checkout/CheckoutLine.tsx
@@ -1,39 +1,36 @@
 'use client'
 
-import { checkoutLines } from '@/constants/testData'
+import { useCheckoutStore } from '@/store/checkout'
 import Image from 'next/image'
 import React from 'react'
 
 const CheckoutLine: React.FC = () => {
+  const checkout = useCheckoutStore((state) => state.checkout)
+
   return (
     <div className="mt-4 bg-white ">
       <div className="bg-gray-50 px-4 py-2">
         <p className="text-lg font-bold">Items</p>
       </div>
       <div className="px-4 py-2">
-        {checkoutLines.map((line) => (
+        {checkout?.lines?.map((line) => (
           <div
             key={line.id}
-            className="mt-5 flex items-center justify-between gap-4 bg-white p-4"
+            className="mt-5 grid grid-cols-4 items-center justify-between gap-4 bg-white p-4"
           >
-            <div className="flex items-center gap-4">
+            <div className="col-span-2 flex items-center gap-4">
               <Image
-                src={line.product_variant.image.url}
+                src={line.product_variant_detail?.product?.thumbnail!}
                 alt="Checkout Line"
                 width={45}
                 height={10}
               />
               <div className="flex flex-col">
-                <p>{line.product_variant.product.name}</p>
-                {line.product_variant.name && (
-                  <p className="text-xs text-gray-400">
-                    Variant: {line.product_variant.name}
-                  </p>
-                )}
+                <p>{line.product_variant_detail?.product?.name}</p>
               </div>
             </div>
             <p className="text-lg font-semibold">
-              ₱{line.product_variant.price}
+              ₱{line.product_variant_detail?.price}
             </p>
             <p>Qty: {line.quantity}</p>
           </div>
