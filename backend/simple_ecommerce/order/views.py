15b3ae14e1d61ebd73133c0caf94c7662b74b182
Martin Josh <martinjosh@vashcorp.com>
2024-01-27 18:37:31 +0800

    Backend update order list ordering

--- backend/simple_ecommerce/order/views.py
+++ backend/simple_ecommerce/order/views.py
@@ -6,9 +6,10 @@ from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from .models import Order
 from .serializers import OrderSerializer
 
+
 # Create your views here.
 class OrderList(generics.ListAPIView):
-    queryset = Order.objects.all()
+    queryset = Order.objects.all().order_by("-created")
     serializer_class = OrderSerializer
     permission_classes = [IsAdminUser]
 
@@ -20,12 +21,11 @@ class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
     def get_permissions(self):
         method = self.request.method
 
-        if method in ['PUT', 'PATCH', 'DELETE']:
+        if method in ["PUT", "PATCH", "DELETE"]:
             self.permission_classes = [IsAdminUser]
         else:
             self.permission_classes = [IsAuthenticated]
         return super(OrderDetail, self).get_permissions()
-
 
     def retrieve(self, request, *args, **kwargs):
         if request.user != self.get_object().user and not request.user.is_staff:
