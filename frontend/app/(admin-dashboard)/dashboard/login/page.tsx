aa29d9cb9a2c3cd0e6a3899cd901e1ab59b062fc
Martin Josh <martinjosh@vashcorp.com>
2024-02-17 00:34:47 +0800

    Add admin login page

---
+++ frontend/app/(admin-dashboard)/dashboard/login/page.tsx
@@ -0,0 +1,25 @@
+import SignInForm from '@/components/forms/SignInForm'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+export default async function DashboardLogin() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <Card className="w-96">
+        <CardHeader>
+          <CardTitle>Sign in</CardTitle>
+          <CardDescription>Enter your credentials to proceed</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInForm />
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
