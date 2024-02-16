5f4ea8307598da64ef64e7e5d721f9a7c4daff5f
Martin Josh <martinjosh@vashcorp.com>
2024-02-17 01:08:19 +0800

    Initialize admin dashboard side navbar

---
+++ frontend/app/(admin-dashboard)/dashboard/layout.tsx
@@ -0,0 +1,46 @@
+import { BaggageClaim, Box, Home, UserCog, Users } from 'lucide-react'
+import React from 'react'
+
+interface DashboardLayoutProps {
+  children: React.ReactNode
+}
+
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    <div className="flex">
+      <div className="mt-16 h-screen px-12">
+        <div className="flex flex-col gap-6">
+          <div className="flex items-center gap-4">
+            <Home size={30} />
+            <p className="text-lg font-semibold">Home</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <Users size={30} />
+            <p className="text-lg font-semibold">Customers</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <Box size={30} />
+            <p className="text-lg font-semibold">Products</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <Box size={30} />
+            <p className="text-lg font-semibold">Brands</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <Box size={30} />
+            <p className="text-lg font-semibold">Categories</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <BaggageClaim size={30} />
+            <p className="text-lg font-semibold">Orders</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <UserCog size={30} />
+            <p className="text-lg font-semibold">Staffs</p>
+          </div>
+        </div>
+      </div>
+      {children}
+    </div>
+  )
+}
---
+++ frontend/app/(admin-dashboard)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="h-screen w-full bg-gray-100">
+      <p>Dashboard</p>
+    </div>
+  )
+}
